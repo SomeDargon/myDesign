95c8833f324dd14016d6a75c4e1abdc4e9e3c176
xiaolong <296569845@qq.com>
2018-06-29 17:22:51 +0800

    update file

---
+++ observer-pattern/src/main/java/com/pattern/Observer.java
@@ -0,0 +1,7 @@
+package com.pattern;
+
+public abstract class Observer {
+    protected Subject subject;
+
+    public abstract void update();
+}
---
+++ observer-pattern/src/main/java/com/pattern/Subject.java
@@ -0,0 +1,36 @@
+package com.pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 主题
+ */
+public class Subject {
+
+    // 观察者对象
+    private List<Observer> observers
+            = new ArrayList<Observer>();
+
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers() {
+        for (Observer observer:observers) {
+            observer.update();
+        }
+    }
+
+
+}
